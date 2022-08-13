af544768bcf4ed6fc757a8008dbadec21c0d5602
19130004 <19130004@st.hcmuaf.edu.vn>
2022-08-13 15:06:59 +0700

    add home

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,18 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-
+import { HomeComponent } from './home/home.component';
+import { AppRoutingModule } from './app-routing.module';
+import {NewsApiService} from "./Service/news-api.service";
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
