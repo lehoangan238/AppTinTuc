ded754bc954521e52c3d906d72bd651693756f35
19130004 <19130004@st.hcmuaf.edu.vn>
2022-08-13 15:15:54 +0700

    pussh

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,15 +5,27 @@ import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { AppRoutingModule } from './app-routing.module';
 import {NewsApiService} from "./Service/news-api.service";
+import {HeaderComponent} from "./header/header.component";
+import {HttpClientModule} from "@angular/common/http";
+import {BrowserAnimationsModule} from "@angular/platform-browser/animations";
+import {FormsModule} from "@angular/forms";
+
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
-
+   HeaderComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
+    BrowserModule,
+    AppRoutingModule,
+    HttpClientModule,
+    BrowserAnimationsModule,
+    FormsModule,
+    // Ng2SearchPipeModule,
+
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/header/header.component.ts
@@ -0,0 +1,28 @@
+import {Component, EventEmitter, Input, OnInit, Output} from '@angular/core';
+import {Router} from "@angular/router";
+
+@Component({
+  selector: 'app-header',
+  templateUrl: './header.component.html',
+  styleUrls: ['./header.component.css']
+})
+export class HeaderComponent implements OnInit {
+showSearch:boolean = false;
+  constructor(private  router : Router) { }
+
+  ngOnInit(): void {
+  }
+  enteredSearchValue: string = '';
+  @Output()
+  searchTextChanged:EventEmitter<string> = new EventEmitter<string>();
+  onSearchTextChange() {
+    this.searchTextChanged.emit(this.enteredSearchValue);
+  }
+  search(){
+    this.router.navigate(['/search',this.enteredSearchValue]);
+  }
+  openSearch(){
+    this.showSearch = !this.showSearch;
+  }
+
+}
